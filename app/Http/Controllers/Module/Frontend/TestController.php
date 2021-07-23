520ff8c7c535aa48d6aa5638378ffca9a9f9bbda
unknown <nguyenquy101192@gmail.com>
2021-07-23 08:27:01 +0700

    redirect frontend

--- app/Http/Controllers/Module/Frontend/TestController.php
+++ app/Http/Controllers/Module/Frontend/TestController.php
@@ -15,6 +15,6 @@ class TestController extends FrontendController
 
     public function index()
     {
-        return $this->_to('bot.index');
+        return $this->_to('backend.login');
     }
 }
