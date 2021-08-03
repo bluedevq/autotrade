a0a621827bfc28a592c1880aff79293df34c76b1
unknown <nguyenquy101192@gmail.com>
2021-08-03 08:16:59 +0700

    update validate method

--- app/Views/layouts/backend/elements/modal.blade.php
+++ app/Views/layouts/backend/elements/modal.blade.php
@@ -1,11 +1,11 @@
-<div class="toast-message-error toast fade position-absolute top-10 end-10 hide" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-body alert-danger">
-        <span class="text-danger toast-message-body"></span><button type="button" class="btn-close position-absolute end-10" data-bs-dismiss="toast" aria-label="Close"></button>
+<div class="toast-message-error border-radius-custom toast fade position-absolute top-10 end-10 bg-danger-custom hide" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-body border-radius-custom">
+        <span class="toast-message-body"></span><button type="button" class="btn-close position-absolute end-10" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
 </div>
-<div class="toast-message-success toast fade position-absolute top-10 end-10 hide" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-body alert-success">
-        <span class="text-success toast-message-body"></span><button type="button" class="btn-close position-absolute end-10" data-bs-dismiss="toast" aria-label="Close"></button>
+<div class="toast-message-success border-radius-custom toast fade position-absolute top-10 end-10 bg-success-custom hide" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-body border-radius-custom">
+        <span class="toast-message-body"></span><button type="button" class="btn-close position-absolute end-10" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
 </div>
 @if(session()->has('success'))
