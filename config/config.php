e9c81a8d45588b28af1fe7f6564e88fa193dbd6b
unknown <nguyenquy101192@gmail.com>
2021-08-19 23:59:45 +0700

    change config

--- config/config.php
+++ config/config.php
@@ -45,7 +45,7 @@ return [
     'aresbo' => [
         // api
         'api_url' => [
-            'captcha_token' => '03AGdBq27jgvS3PSiB_IiRCWqGcC-sANBQu9ha6axMhMw1ZszFeLELEJZpzt5z1-53gmy-rfLN-iwMVtg4tEJ_GHmMRwKAg5CcisjEzlWfxAFtAHZML0KEmhrRZK0F_F9VzhNbEGW2mQ7OipxMuubvg6a3mwQZ0hXfotyZnDSxWoZZW28sWvJZNCQVudvnFweFuMxbw6Xrb-kybfotNECD_sK2PGZBephuR2n_sby5HkI9UgS4QLI2zffAgedbNw0eEXO_JEPfKr8q-ZBuLnzrwBH8sciTGvJmL0t-E0AbJWgBSi62QSsEY0sl9plpyDmpNX6psGwk6GDN1MKRWno_BaX2xb7aP2keqA7Z6bLF1kihioOcUgCdrVzySPfuKyb440aly1ComIaJFaHc9tz9PGKhDhx52idYD-MmpvyVa19n-IJbVfO-nP3zUUgghqoRsFosoliW2WFv',
+            'captcha_token' => env('GOOGLE_CAPTCHA_TOKEN'),
             'get_token_url' => 'https://aresbo.com/api/auth/auth/token',
             'get_token2fa_url' => 'https://aresbo.com/api/auth/auth/token-2fa',
             'get_overview' => 'https://aresbo.com/api/wallet/binaryoption/user/overview',
